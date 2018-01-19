c54edf60ca4b0c02fa26c59d57d764fdcb0af04e
Moldovan Alenxadru <moldovan_alexandru97@yahoo.com>
2018-01-19 12:27:42 +0200

    Last changes done. Toy Interpreter implementation done.

---
+++ Toy Interpreter/src/Model/Command/ExitCommand.java
@@ -0,0 +1,21 @@
+/**************************************************************************************************
+ *       Copyright(c):                                                                            *
+ *                         Program made by @Moldovan Alexandru-Vasile.                            *
+ *                                                                                                *
+ **************************************************************************************************/
+
+package Model.Command;
+
+public class ExitCommand extends Command {
+
+    public ExitCommand(String key, String description)
+    {
+        super(key, description);
+    }
+
+    @Override
+    public void execute()
+    {
+        System.exit(0);
+    }
+}
---
+++ Toy Interpreter/src/Model/Statement/Statement.java
@@ -0,0 +1,12 @@
+/**************************************************************************************************
+ *       Copyright(c):                                                                            *
+ *                         Program made by @Moldovan Alexandru-Vasile.                            *
+ *                                                                                                *
+ **************************************************************************************************/
+
+package Model.Statement;
+
+public interface Statement
+{
+    PrgState execute(PrgState p);
+}
